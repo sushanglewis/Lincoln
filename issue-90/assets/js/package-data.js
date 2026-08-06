00ebcb14fb9957263d9217f9944da87473914a1f
White-Lewis <sushanglewiswork@163.com>
2026-08-06 15:35:17 +0800

    Delete issue-90 directory

--- issue-90/assets/js/package-data.js
+++
@@ -1,8 +0,0 @@
-window.LINC_PACKAGE = {
-  "process_slug": "issue-90",
-  "issue_number": "90",
-  "current_stage": "ingest",
-  "status": "validation_failed",
-  "generated_at": "2026-07-31T05:25:36Z",
-  "nav": []
-};
